c5d7b5f76d93b3bbdaf3bcad8c88137311debe71
AngelVI13 <a.v.iliev13@gmail.com>
2021-12-31 13:57:59 +0000

    Added 2 player random play

--- tic-tac-toe.fs
+++ tic-tac-toe.fs
@@ -15,12 +15,17 @@ ROW-SIZE ROW-SIZE * CONSTANT BOARD-SIZE
 1  CONSTANT X
 -1 CONSTANT O
 
+VARIABLE CURRENT_PLAYER
 VARIABLE BOARD BOARD-SIZE 1- CELLS ALLOT
 
-: RESET ( -- ) BOARD BOARD-SIZE CELLS ERASE ;
+: RESET-PLAYER X CURRENT_PLAYER ! ;
+: RESET-BOARD ( -- ) BOARD BOARD-SIZE CELLS ERASE ;
 : BOARD-FIELD ( index -- address ) CELLS BOARD + ;
-: MARK-X! ( addr -- ) X SWAP ! ;
-: MARK-O! ( addr -- ) O SWAP ! ;
+\ TODO is there something like *! (similar to +!) ??
+: CHANGE-PLAYER ( -- ) CURRENT_PLAYER @ -1 * CURRENT_PLAYER ! ;
+: MARK! ( addr -- ) 
+    CURRENT_PLAYER @ SWAP ! 
+    CHANGE-PLAYER ;
 : _RANDOM ( -- n) BOARD-SIZE RANDOM ;
 : DISPLAY-BOARD ( -- ) 
     CR 
@@ -41,16 +46,37 @@ VARIABLE BOARD BOARD-SIZE 1- CELLS ALLOT
             DROP FALSE LEAVE
         THEN LOOP ;
 
-: SPAM-RANDOM-MARKS ( -- ) 100 0 DO _RANDOM BOARD-FIELD MARK-X! BOARD-FULL IF LEAVE ELSE DISPLAY-BOARD THEN LOOP ;
+: SPAM-RANDOM-MARKS ( -- ) 100 0 DO _RANDOM BOARD-FIELD MARK! BOARD-FULL IF LEAVE ELSE DISPLAY-BOARD THEN LOOP ;
+: RANDOM-EMPTY ( -- n ) 
+    \ Get a random empty field from board, assumes that board is NOT full
+    \ TODO how to handle case when board is full ??
+    BEGIN 
+        _RANDOM DUP BOARD-FIELD @ 
+        EMPTY = IF
+            \ TODO Why do I have to use EXIT here and not LEAVE 
+            \ (leave causes "Invalid memory access" error) 
+            EXIT 
+        ELSE
+            DROP
+        THEN AGAIN ; 
+: EMPTY-FIELD ( -- ) 
+    BOARD-SIZE 0 DO 
+        I BOARD-FIELD @ EMPTY 
+        = IF 
+            I LEAVE 
+        THEN LOOP ;
+: PLAY-GAME ( -- ) 
+    RESET-BOARD
+    RESET-PLAYER
+    DISPLAY-BOARD
+    BEGIN
+        RANDOM-EMPTY BOARD-FIELD MARK! 
+        DISPLAY-BOARD
+        BOARD-FULL
+    UNTIL
+    ." Game Over! " ;
 
-RESET
-DISPLAY-BOARD
-BOARD-FULL .
-CR
-\ _RANDOM BOARD-FIELD MARK-X!
-SPAM-RANDOM-MARKS
-DISPLAY-BOARD
-BOARD-FULL .
+PLAY-GAME
 
 \ Use the following command to run this file
 \ gforth tic-tac-toe.fs -e bye
